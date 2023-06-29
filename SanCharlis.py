617a7ee86c261789da001b720c003c4ddfcb9d2e
Jorgereyes27 <137914872+Jorgereyes27@users.noreply.github.com>
2023-06-29 13:09:12 -0400

    prueba 3 Jorge reyes

---
+++ SanCharlis.py
@@ -0,0 +1,25 @@
+""""
+Opción 1
+●	Registrar: Corresponde a guardar ciertos datos de un alumno, entre ellos: 
+Rut, nombre completo, edad, género, promedio de notas, fecha matricula y nombre del apoderado.
+Además, debe verificar que el rut no se encuentre repetido, el nombre del alumno considere entre 2 y 30 caracteres y la edad sea mayor o igual a 4 años.
+
+Opción 2
+●	Buscar: Corresponde buscar un alumno por su rut y mostrar toda su información almacenada.
+
+Opción 3
+●	Imprimir certificados: Esta opción permite imprimir los certificados de
+ 1) anotaciones de un alumno, 
+ 2) certificado de notas y 
+ 3) certificado de alumno regular. 
+ Estos certificados deben ser previamente ingresados con valores aleatorios. 
+ Al imprimir el certificado, debe mostrar el nombre del certificado, rut y nombre del alumno.
+"""
+
+import numpy
+
+alumno = numpy.empty([10,7])
+
+
+
+print(alumno)
